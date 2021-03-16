67d9ce0d1d3215c3af5e260866a5e65fc439b9f8
Puzzlebox-IMT <76156208+Puzzlebox-IMT@users.noreply.github.com>
2021-03-16 22:13:01 +0100

    Add charlieplexing for LEDs

--- WifiEnigma/mainESPWifi/mainESPWifi.ino
+++ WifiEnigma/mainESPWifi/mainESPWifi.ino
@@ -42,6 +42,10 @@ char encryptedPasswordArray[17];
 char secretKeyArray[17];  
 //secretKey.toCharArray(secretKeyArray, secretKey.length()+1);
 
+//Variable for charlieplexing
+int A = 2;
+int B = 3;
+int C = 4;
 
 //Variables for the whole game functionning
 char* face_id = "1-WiFi";
@@ -62,7 +66,68 @@ void setup() {
 
   lcd.init();   // initializing the LCD
   lcd.backlight(); // Enable or Turn On the backlight 
+  
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT); 
+  pinMode(C, OUTPUT);  
 
+}
+
+void setLED(int ledNum)
+{
+  if(ledNum == 1)
+  {
+     pinMode(A, OUTPUT);
+     pinMode(B, INPUT);  // changed to input to trigger tri state.
+     pinMode(C, OUTPUT); 
+     digitalWrite(A, HIGH);             
+     digitalWrite(C, LOW);
+  }
+
+  if(ledNum == 2)
+  {
+     pinMode(A, OUTPUT);
+     pinMode(B, INPUT); 
+     pinMode(C, OUTPUT);           
+     digitalWrite(A, LOW);             
+     digitalWrite(C, HIGH);
+  }
+
+  if(ledNum == 3)
+  { 
+     pinMode(A, OUTPUT);
+     pinMode(B, OUTPUT);
+     pinMode(C, INPUT);        
+     digitalWrite(A, HIGH);
+     digitalWrite(B, LOW);        
+  }
+
+  if(ledNum == 4)
+  {
+     pinMode(A, OUTPUT);
+     pinMode(B, OUTPUT);
+     pinMode(C, INPUT);           
+     digitalWrite(A, LOW);
+     digitalWrite(B,HIGH);
+  }
+
+  if(ledNum == 5)
+  {
+     pinMode(A, INPUT);
+     pinMode(B, OUTPUT);
+     pinMode(C, OUTPUT);               
+     digitalWrite(B, HIGH);
+     digitalWrite(C, LOW);
+  }
+
+  if(ledNum == 6)
+  {
+     pinMode(A, INPUT);
+     pinMode(B, OUTPUT);
+     pinMode(C, OUTPUT);  
+     digitalWrite(B, LOW);
+     digitalWrite(C, HIGH);
+  }
 }
 
 
